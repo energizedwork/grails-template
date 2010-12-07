9d17c3e0ce9957eb7e79e567dd7eec7aa430fd23
Marcin Floryan <marcin@energizedwork.com>
2010-12-07 11:57:12 +0000

    All default plugins into BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,29 +3,35 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.5'
-    }
+	inherits("global") {
+		// uncomment to disable ehcache
+		// excludes 'ehcache'
+	}
+	log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+	repositories {
+		grailsPlugins()
+		grailsCentral()
+		grailsHome()
+		mavenCentral()
+		mavenLocal()
+		mavenRepo "http://build.disney.ew/artifactory/repo"
+	}
+	dependencies {
+		// provided "mysql:mysql-connector-java:5.1.8"
+		runtime "postgresql:postgresql:8.2-507.jdbc4"
+	}
+	plugins {
+		compile ":hibernate:1.3.5"
+		compile ":joda-time:1.2-SNAPSHOT"
+		compile ":cache-headers:1.1.2"
+		build ":bean-fields:1.0-RC3"
+		build ":tomcat:1.3.5"
+		build ":liquibase:1.9.3.3"
+		runtime ":plugins.jquery:1.4.2.5"
+		runtime ":plugins.jquery-ui:1.8.4.3"
+		test ":spock:0.5-groovy-1.7-SNAPSHOT"
+		test ":build-test-data:1.1.1"
+		test ":fixtures:1.0.1"
+		test "com.energizedwork:remote-fixtures:1.0.1-SNAPSHOT"
+	}
 }
