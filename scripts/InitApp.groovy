9ff1bb03fc42e09c3011542789966e8bfb80e2f6
Kris Lander <kris@energizedwork.com>
2010-12-08 15:25:52 +0000

    [kris|thando] implemented updating application properties in  init-app script

--- scripts/InitApp.groovy
+++ scripts/InitApp.groovy
@@ -2,12 +2,22 @@ includeTargets << grailsScript("_GrailsArgParsing")
 includeTargets << grailsScript("TestApp")
 
 target(main: "The description of the script goes here!") {
-    depends(parseArguments, renameApplication)
+    depends(parseArguments, renameApplication, 'default')
 }
 
 target(renameApplication: "renames the template app to given project name") {
 	String appName = argsMap.appName
-	event "StatusUpdate", ["Renaming template project to ${appName}"]
+	event "StatusUpdate", ["\nRenaming template project to ${appName}\n"]
+
+	updateApplicationProperties(appName)
+}
+
+void updateApplicationProperties(String appName) {
+	Properties props = new Properties()
+	props.load new FileInputStream('application.properties')
+
+	props.setProperty("app.name", appName)
+	props.store(new FileOutputStream('application.properties'), "Updating appName to ${appName}")
 }
 
 setDefaultTarget(main)
